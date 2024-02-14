b1ba2b5f3dbf0ccac0fb49c38254f9f223cc42bb
Vinicius Dantas e Melo <vdmelo@gmail.com>
2024-02-14 18:54:31 -0300

    Feature/vinicius/teste (#1)
    * New branch
    
    * New branch

--- src/main/java/com/batmelo/awsteste1/controller/NewController.java
+++ src/main/java/com/batmelo/awsteste1/controller/NewController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewController {
 
-    @GetMapping("teste")
+    @GetMapping("new-teste")
     public String teste() {
-        return "New Controller";
+        return "New Controller v2.";
     }
 }
