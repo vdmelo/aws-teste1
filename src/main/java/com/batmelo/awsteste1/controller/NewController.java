b7af3b8b6fb0c7668be68540bbd8a98d5c09a15d
Vinicius Melo <vdmelo@gmail.com>
2024-02-14 18:53:31 -0300

    New branch

--- src/main/java/com/batmelo/awsteste1/controller/NewController.java
+++ src/main/java/com/batmelo/awsteste1/controller/NewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewController {
 
-    @GetMapping("teste")
+    @GetMapping("new-teste")
     public String teste() {
         return "New Controller v2.";
     }
