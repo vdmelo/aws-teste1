7d4a418ed3ff4e9e54b9eeacdf3691a425fb3392
Vinicius Melo <vdmelo@gmail.com>
2024-02-19 20:44:55 -0300

    Change docker build

--- aws-cdk/lib/aws-cdk-stack.ts
+++ aws-cdk/lib/aws-cdk-stack.ts
@@ -55,7 +55,7 @@ export class AwsTeste1BuildDeployStack extends cdk.Stack {
       buildSpec: codebuild.BuildSpec.fromSourceFilename("aws-cdk/app/buildspec.yaml"),
       source: codebuild.Source.codeCommit({ repository: codeRepo }),
       environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
         privileged: true,
         environmentVariables: {
           AWS_ACCOUNT_ID: { value: process.env?.CDK_DEFAULT_ACCOUNT || "" },
